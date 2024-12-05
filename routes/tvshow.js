a2df0f8e03f2cd5f60a2d498a55a1ea03e849c19
sherynnnn <142745064+sherynnnn@users.noreply.github.com>
2024-12-05 14:33:47 +0800

    Update tvshow.js

--- routes/tvshow.js
+++ routes/tvshow.js
@@ -1,42 +1,129 @@
 const express = require("express");
-//create a router for movies
+//create a router for tvshows
 const router = express.Router();
 
 // load the models
-const Tvshow= require("../models/tvshow");
+const { 
+  getTvshows,
+  getTvshow,
+  addNewTvshow,
+  updateTvshow,
+  deleteTvshow,
+} = require("../controller/tvshow");
 
 // create the routes
 
-// get all the movies. Pointing to /movies
+// get all the tvshows. Pointing to /tvshows
 router.get("/", async (req, res) => {
+  try{
   const genre = req.query.genre;
   const rating = req.query.rating;
   const premiere_year = req.query.premiere_year;
-  // create a container for filter
-  let filter = {};
-  // if genre exists, pass it to the filter container
-  if (genre) {
-    filter.genre = genre;
-  }
-  // if rating exist, pass it into the filter container
-  if (rating) {
-    filter.rating = { $gt: rating };
-  }
-  // if director exist, pass into the filter container
-  if (premiere_year) {
-    filter.premiere_year = { $gt: premiere_year};
-  }
-
-  // apply filter in .find()
-  const tvshows = await Tvshow.find(filter);
-  res.send(tvshows);
+  // use the gettvshows from the controller to laod the movies data
+  const tvshows = await getTvshows(genre, rating, premiere_year);
+  res.status(200).send(tvshows);
+}catch (error) {
+  res.status(400).send({
+    error: error._message,
+  });
+}
 });
 
 // get one show by id
 router.get("/:id", async (req, res) => {
+  try{
   const id = req.params.id;
-  const tvshows = await Tvshow.findById(id);
-  res.send(tvshows);
+  const tvshows = await getTvshow.findById(id);
+  res.status(200).send(tvshows);
+  }catch(error){
+    res.status(400).send({
+      error: error._message,
+    });
+  }
 });
+
+// add shows
+// POST http://localhost:5555/tvshows
+router.post("/", async (req, res) => {
+  try {
+    // retrieve the data from req.body
+    const title = req.body.title;
+    const creator = req.body.creator;
+    const premiere_year = req.body.premiere_year;
+    const end_year = req.body.end_year;
+    const season = req.body.season;
+    const genre = req.body.genre;
+    const rating = req.body.rating;
+
+    // check for error
+    if (!title || !creator || !premiere_year ||!end_year || !season || !genre || !rating) {
+      return res.status(400).send({
+        error: "Required data is missing",
+      });
+    }
+     // pass in all the data to addNewMovie function
+     const newTvshow = await addNewTvshow(
+      title,
+      creator,
+      premiere_year,
+      end_year,
+      season,
+      genre,
+      rating
+    );
+    res.status(200).send(newTvshow);
+  } catch (error) {
+    // if there is an error, return the error code
+    res.status(400).send({
+      error: error._message,
+    });
+  }
+});
+
+// update shows
+// PUT http://localhost:5555/tvshows/9kdm40ikd93k300dkd3o
+router.put("/:id", async (req, res) => {
+  try{
+    const title = req.body.title;
+    const creator = req.body.creator;
+    const premiere_year = req.body.premiere_year;
+    const end_year = req.body.end_year;
+    const season = req.body.season;
+    const genre = req.body.genre;
+    const rating = req.body.rating;
+    // pass in the data into the updateMovie function
+    const updatedTvshow = await updateTvshow(
+      title,
+      creator,
+      premiere_year,
+      end_year,
+      season,
+      genre,
+      rating
+    );
+    res.status(200).send(updatedTvshow);
+  }catch (error) {
+    restart.status(400).send({
+      error: error_message,
+    });
+  }
+});
+
+// delete shows
+// DELETE http://localhost:5555/tvshows/9kdm40ikd93k300dkd3o
+router.delete("/:id", async (req, res) => {
+  try{
+    const id = req.params.id;
+     // trigger the deleteMovie function
+     await deleteTvshow(id);
+     res.status(200).send({
+       message: `Movie with the provided id #${id} has been deleted`,
+     });
+   } catch (error) {
+     res.status(400).send({
+       error: error._message,
+     });
+   }
+  });
 
 module.exports = router;
